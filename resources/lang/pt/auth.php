a3592dd2f335e2bcdb53a404f6a07ea360a2dd2a
Maisa Gabriela Rodrigues <maisagabirodrigues@gmail.com>
2024-12-14 22:35:20 -0300

    fix: language app

---
+++ resources/lang/pt/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Usuário ou senha inválidos.',
+    'password' => 'Usuário ou senha inválidos.',
+    'throttle' => 'Muitas tentativas de login. Por favor, tente novamente em :seconds segundos.',
+
+];
